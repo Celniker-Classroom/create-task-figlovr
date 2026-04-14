1c341476ad609c8194f937ef747e281da0219a7c
figlovr <kavya7karthik@gmail.com>
2026-04-14 20:51:27 +0000

    fixed html and created functions for the buttons

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // add javascript here
+
+
+
+function currency(change) {
+
+} 
+
+document.getElementById("entry").addEventListener("click", function() {
+    let dropdown = document.getElementsByName("country");
+    
+})
